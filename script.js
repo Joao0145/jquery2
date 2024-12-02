030e4621aedae11772181dcd6c05c84af27d88e1
João Victor Alves <160748355+Joao0145@users.noreply.github.com>
2024-12-02 19:16:36 -0300

    subir repositório
    subir repositório

---
+++ script.js
@@ -0,0 +1,12 @@
+$('#btnClick').click(function() {
+    $('h1').css('color', 'red')
+});
+
+$('#btnClick').click(function() {
+    $('#mensagem').fadeIn(1000); // Faz o texto desaparecer gradualmente
+});
+
+
+$('#btnClick').click(function() {
+    $('mensagem').html('<strong>Voce clicou no botao com jQuery!</strong>')
+});
